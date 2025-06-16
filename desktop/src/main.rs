5543fbd6f3879cb3f8679679f6f99de5e018ba4e
Doug Anderson444 <douganderson444@gmail.com>
2025-06-16 20:55:05 -0300

    web toml

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -5,7 +5,7 @@ use dioxus::prelude::*;
 
 use ui::{Hero, StorageProvider};
 
-const MAIN_CSS: Asset = asset!("/assets/main.css");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
     dioxus::launch(App);
@@ -22,7 +22,7 @@ fn App() -> Element {
 
     rsx! {
         // Global app resources
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
 
         Hero {}
 
